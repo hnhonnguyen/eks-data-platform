3350b9124a2dbcb63c7ccb6ca43b3daab20b4ecb
Nhon Nguyen <hnhonnguyen@Nhons-MacBook-Pro.local>
2024-07-22 15:39:36 +0700

    commit

--- iam_role.tf
+++ iam_role.tf
@@ -1,15 +1,15 @@
-# data "aws_iam_role" "ClusterRole" {
-#   name = "vendor1-service-role-cluster"
-# }
+data "aws_iam_role" "ClusterRole" {
+  name = "vendor1-service-role-cluster"
+}
 
-# data "aws_iam_role" "CoreNodeGroup" {
-#   name = "vendor1-service-role-core-eks-node-group"
-# }
+data "aws_iam_role" "CoreNodeGroup" {
+  name = "vendor1-service-role-core-eks-node-group"
+}
 
-# data "aws_iam_role" "KarpenterNode" {
-#   name = "vendor1-service-role-karpenter-node"
-# }
+data "aws_iam_role" "KarpenterNode" {
+  name = "vendor1-service-role-karpenter-node"
+}
 
-# data "aws_iam_role" "Karpenter" {
-#   name = "vendor1-service-role-karpenter"
-# }
+data "aws_iam_role" "Karpenter" {
+  name = "vendor1-service-role-karpenter"
+}
