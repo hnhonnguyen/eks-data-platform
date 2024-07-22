3c607033b16ea87ba9ac256a47826a08432266af
Nhon Nguyen <hnhonnguyen@Nhons-MacBook-Pro.local>
2024-07-22 15:30:10 +0700

    commit

--- main.tf
+++ main.tf
@@ -9,37 +9,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  # token                  = data.aws_eks_cluster_auth.this.token
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--region", local.region, "--cluster-name", module.eks.cluster_name]
-  }
-}
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#   # token                  = data.aws_eks_cluster_auth.this.token
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "aws"
+#     # This requires the awscli to be installed locally where Terraform is executed
+#     args = ["eks", "get-token", "--region", local.region, "--cluster-name", module.eks.cluster_name]
+#   }
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.this.token
+#   }
+# }
 
-provider "kubectl" {
-  apply_retry_count      = 30
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.this.token
-}
+# provider "kubectl" {
+#   apply_retry_count      = 30
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#   load_config_file       = false
+#   token                  = data.aws_eks_cluster_auth.this.token
+# }
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster_auth" "this" {
+#   name = module.eks.cluster_name
+# }
 
 # This ECR "registry_id" number refers to the AWS account ID for us-west-2 region
 # if you are using a different region, make sure to change it, you can get the account from the link below
