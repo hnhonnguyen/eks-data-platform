4cfe704d198694d35eedb9698b7a0a9087d38544
Nhon Nguyen <hnhonnguyen@Nhons-MacBook-Pro.local>
2024-07-22 15:00:23 +0700

    commit

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ module "eks" {
   aws_auth_roles = [
     # We need to add in the Karpenter node IAM role for nodes launched by Karpenter
     {
-      rolearn  = module.eks_blueprints_addons.karpenter.node_iam_role_arn
+      rolearn  = data.aws_iam_role.KarpenterNode
       username = "system:node:{{EC2PrivateDNSName}}"
       groups = [
         "system:bootstrappers",
