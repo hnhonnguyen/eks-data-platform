9a42e5e59c0aa5be03939e8ad91b3cc21dcbb533
Nhon Nguyen <hnhonnguyen@Nhons-MacBook-Pro.local>
2024-07-22 14:45:58 +0700

    commit

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ output "ecr_username" {
   value = data.aws_ecr_authorization_token.token.user_name
 }
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" { provider = aws.ecr }
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 data "aws_partition" "current" {}
